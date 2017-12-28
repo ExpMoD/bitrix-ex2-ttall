2bc48a03c3f246e4c43035a044f99e0db9a6f9f9
“Kudakov <kudakovandrey@gmail.com>
2017-12-28 12:08:11 +0300

    [ex2-88]

---
+++ ex2/time-control/index.php
@@ -0,0 +1,9 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
+$APPLICATION->SetTitle("Оценка производительности");
+?><h2>Нагрузка</h2>
+ Страница:&nbsp;<a href="http://bitrix-ex2-ttall/bitrix/admin/perfmon_hit_list.php?lang=ru&set_filter=Y&find_script_name=%2Fnews%2Findex.php">/news/index.php</a><br>
+ Доля:&nbsp;13.79%<br>
+<h2>КЕШ</h2>
+ По умолчанию: <b>258 кб</b><br>
+ При помещении в него только данных, необходимых в некешируемой части: <b>245 кб</b><br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
