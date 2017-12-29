8e7d4d62b22fb86050c4959398516bd27ba3008c
“Kudakov <kudakovandrey@gmail.com>
2017-12-29 09:12:06 +0300

    [ex2-54]

--- local/php_interface/init.php
+++ local/php_interface/init.php
@@ -11,3 +11,57 @@ if (file_exists($_SERVER['DOCUMENT_ROOT'] . "/local/php_interface/include/EventH
 
 if (file_exists($_SERVER['DOCUMENT_ROOT'] . "/local/php_interface/include/Seo.php"))
     require_once($_SERVER['DOCUMENT_ROOT'] . "/local/php_interface/include/Seo.php");
+
+
+define("LOG_FILENAME", $_SERVER["DOCUMENT_ROOT"]."/log.txt");
+
+
+function CheckUserCount()
+{
+    $date = new DateTime();
+    $date = \Bitrix\Main\Type\DateTime::createFromTimestamp($date->getTimestamp());
+
+    $lastDate = COption::GetOptionString("main", 'last_date_agent_checkUserCount');
+
+
+    if ($lastDate) {
+        $arFilter = array('>=DATE_REGISTER' => $lastDate);
+    } else {
+        $arFilter = array();
+    }
+
+    $rsUsers = CUser::GetList($by = "DATE_REGISTER", $order = "ASC", $arFilter);
+
+    $arUsers = array();
+
+    while ($user = $rsUsers->Fetch()) {
+        $arUsers[] = $user;
+    }
+
+    if (! $lastDate) {
+        $lastDate = $arUsers[0]['DATE_REGISTER'];
+    }
+
+    $difference = intval(abs(
+        strtotime($lastDate) - strtotime($date->toString())
+    ));
+
+    $days = round($difference / (3600 * 24));
+    $countUsers = count($arUsers);
+
+    $rsAdmins = CUser::GetList($by = "ID", $order = "ASC", array('GROUPS_ID' => 1));
+
+
+    while ($admin = $rsAdmins->Fetch()) {
+        CEvent::Send('COUNT_REGISTERED_USERS', 's1', array(
+            'EMAIL_TO' => $admin['EMAIL'],
+            'COUNT_USERS' => $countUsers,
+            'COUNT_DAYS' => $days,
+        ), "Y", "29");
+
+    }
+
+    COption::SetOptionString("main", 'last_date_agent_checkUserCount', $date->toString());
+
+    return "CheckUserCount();";
+}
