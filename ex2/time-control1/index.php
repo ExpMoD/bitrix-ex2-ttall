c0827c013280baf54d3cb380272da718208e1ee6
“Kudakov <kudakovandrey@gmail.com>
2018-01-30 13:41:27 +0300

    [ex2-10] Исправление

--- ex2/time-control1/index.php
+++ ex2/time-control1/index.php
@@ -2,8 +2,8 @@
 require($_SERVER["DOCUMENT_ROOT"]."/bitrix/header.php");
 $APPLICATION->SetTitle("Оценка производительности 2");
 ?><h2>Нагрузка</h2>
- Страница:&nbsp;<a href="http://bitrix-ex2-ttall/bitrix/admin/perfmon_hit_list.php?lang=ru&set_filter=Y&find_script_name=%2Fbitrix%2Furlrewrite.php">/bitrix/urlrewrite.php</a><br>
- Доля:&nbsp;37.19%<br>
+ Страница:&nbsp;<a href="http://bitrix-ex2-ttall/bitrix/admin/perfmon_hit_list.php?lang=ru&set_filter=Y&find_script_name=%2Fbitrix%2Furlrewrite.php">/products/index.php</a><br>
+ Доля:&nbsp;20.53%<br>
 <h2>САМЫЙ НАГРУЖАЕМЫЙ КОМПОНЕНТ</h2>
  Название:&nbsp;bitrix:catalog<br>
  Время:&nbsp;0.0773 с<br><?require($_SERVER["DOCUMENT_ROOT"]."/bitrix/footer.php");?>
