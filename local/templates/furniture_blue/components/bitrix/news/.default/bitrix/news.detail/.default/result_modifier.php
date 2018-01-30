486447f28d89da5c660d0fa8bb17201dc700e2d5
“Kudakov <kudakovandrey@gmail.com>
2018-01-30 13:00:42 +0300

    [ex2-108] Задан arSelect

--- local/templates/furniture_blue/components/bitrix/news/.default/bitrix/news.detail/.default/result_modifier.php
+++ local/templates/furniture_blue/components/bitrix/news/.default/bitrix/news.detail/.default/result_modifier.php
@@ -1,15 +1,19 @@
 <?php
 
 if ($arParams['IBLOCK_CANONICAL_ID'] && CModule::IncludeModule('iblock')) {
+    $rsSelect = array(
+        'NAME'
+    );
+
     $rsFilter = array(
         "IBLOCK_ID" => $arParams['IBLOCK_CANONICAL_ID'],
         "PROPERTY_NEWS" => $arResult['ID']
     );
 
-    $canonical = CIBlockElement::GetList(array(), $rsFilter)->Fetch()['NAME'];
+    $canonical = CIBlockElement::GetList(array(), $rsFilter, false ,false, $rsSelect)->Fetch();
 
     if ($canonical) {
-        $arResult['CANONICAL_LINK'] = $canonical;
+        $arResult['CANONICAL_LINK'] = $canonical['NAME'];
         $this->__component->SetResultCacheKeys(array('CANONICAL_LINK'));
     }
 }
